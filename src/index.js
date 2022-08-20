aa0c9e771a8c0d313de6dda35f771f32e6a5e11d
Ziabeher Phillips <zabq02@gmail.com>
2022-08-19 21:40:44 -0400

    Signup page completed

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import SignUp from './components/Sign-Up/SignUp.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/signup" element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
+);
